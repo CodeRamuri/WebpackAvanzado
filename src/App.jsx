9248fa16b4423c4cef83fae57a99f2792e81c5c6
@CodeRamuri <coderamuri@gmail.com>
2021-03-10 17:48:32 -0600

    Termine el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import "./App.css";
+import "./App.scss";
+import logo from "./react.svg";
 
 function App() {
   const [name, setName] = useState("");
   return (
     <div className="app">
       <h1>
-        Hola React
+        Hola chimicuiles de React 
         <img src={logo} width="25" alt="react logo" />
       </h1>
       <div>
